9baa87136d3d68b67fa1a4771794a9901a1d2cf3
Atharv Sonwane <atharvs.twm@gmail.com>
2025-10-30 13:17:38 -0400

    further

---
+++ setup.sh
@@ -0,0 +1,17 @@
+export PATH="$HOME/.local/bin:$PATH"
+
+git clone https://github.com/threewisemonkeys-as/R2E-Gym.git
+cd R2E-Gym
+git checkout fixed-data-alta-cluster
+git submodule update --init --recursive
+pip install --user -e .
+cd ..
+
+git clone https://github.com/threewisemonkeys-as/verl.git
+cd verl
+git fetch 
+git checkout main 
+cd ..
+
+pip install --user -e ./verl[vllm]
+pip install --user -e .
