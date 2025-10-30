d083226e599a1deafa6152b34f5a75b5f1ae8fc2
Atharv Sonwane <atharvs.twm@gmail.com>
2025-10-30 14:24:00 -0400

    change r2egym

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ export PATH="$HOME/.local/bin:$PATH"
 
 git clone https://github.com/threewisemonkeys-as/R2E-Gym.git
 cd R2E-Gym
-git checkout fixed-data-alta-cluster
+git checkout mk-k8s
 git submodule update --init --recursive
 pip install --user -e .
 cd ..
